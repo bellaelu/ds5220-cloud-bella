fcdd26841634270a821b6efe440b9a85cf93e8eb
bellalu <bellaelu26@gmail.com>
2026-02-18 12:08:31 -0500

    merge conflicts

--- reference-iac/bootstrapping/launch-3.sh
+++ reference-iac/bootstrapping/launch-3.sh
@@ -5,7 +5,7 @@
 aws ec2 run-instances \
   --image-id ami-0b6c6ebed2801a5cb \
   --count 1 \
-  --instance-type t3.large \
-  --key-name ds5220 \
-  --security-group-ids sg-066412c1406b41de3 \
+  --instance-type t3.micro \
+  --key-name ds5220awssshkey \
+  --security-group-ids sg-027202f144b2a1ca8 \
   --user-data file://bootstrap-3.sh
